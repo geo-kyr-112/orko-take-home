321a9ad432cd3d6084a84e49315fb6dfb6dcd853
Geo Amos <GeoAmos@Geo-Amos-MB.local>
2022-01-24 10:46:09 -0800

    ORKO-TH | Simplfying CsvObject and adding summaries/comments

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace CsvParser
         private class CsvObject
         {
             public string HeaderRow = string.Empty;
-            public Dictionary<string, string> RowKeyToRowMap = new();
+            public List<string> Rows = new();
         }
 
         static void Main(string[] args)
@@ -22,8 +22,15 @@ namespace CsvParser
             WriteCsvObject(sortedCsvObject);
         }
 
+        /// <summary>
+        /// Reads a csv file and parses it to an object containing a header and list of ordered rows
+        /// </summary>
+        /// <param name="csvFilePath">The file path</param>
+        /// <param name="columnName">The name of the column to sort by</param>
+        /// <returns>CsvObject.  Contains header row as a string and a list of ordered rows</returns>
         private static CsvObject ParseCsvObject(string csvFilePath, string columnName)
         {
+            // Validate that file exists
             if (!File.Exists(csvFilePath))
             {
                 throw new Exception("File does not exist");
@@ -31,10 +38,12 @@ namespace CsvParser
 
             string[] lines = File.ReadAllLines(csvFilePath);
             List<string> headers = lines[0].Split(',').ToList();
+            // Validate that ordering column exists in the header
             if (!headers.Contains(columnName))
             {
                 throw new Exception("Column does not exist in file header");
             }
+            // Validate that the header value is unique by comparing the length of the headers as a total to a hash set of the headers
             HashSet<string> headerSet = new(headers);
             if (headers.Count != headerSet.Count)
             {
@@ -42,6 +51,8 @@ namespace CsvParser
             }
             int keyColumnIndex = headers.IndexOf(columnName);
 
+            // Creates a dictionary where the key field is the one to order by and value is the entire row
+            // This allows sorting based on the key while preserving the entire row
             Dictionary<string, string> csvMap = new();
             List<int> errorLines = new();
             for (int i = 1; i < lines.Length; i++)
@@ -56,25 +67,31 @@ namespace CsvParser
                     csvMap.Add(line[keyColumnIndex], lines[i]);
                 }
             }
+            // Validate that there are no lines with an incorrect number of columns
             if (errorLines.Count > 0)
             {
                 throw new Exception($"Lines {string.Join(", ", errorLines)} do not have correct column count");
             }
+            // Sorts the dictionary in descending order of the key column
             csvMap = csvMap.OrderByDescending(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
 
             return new CsvObject
             {
                 HeaderRow = lines[0],
-                RowKeyToRowMap = csvMap
+                Rows = csvMap.Values.ToList()
             };
         }
 
+        /// <summary>
+        /// Prints to console a CsvObject
+        /// </summary>
+        /// <param name="csvObject">Object containing a header and list of ordered rows</param>
         private static void WriteCsvObject(CsvObject csvObject)
         {
             Console.WriteLine(csvObject.HeaderRow);
-            foreach (var line in csvObject.RowKeyToRowMap)
+            foreach (var line in csvObject.Rows)
             {
-                Console.WriteLine(line.Value);
+                Console.WriteLine(line);
             }
         }
     }
